e1d0707ca8c36403b083d88a263871a9d5f0bc1a
Dimakis Kostas <costasdhm2002@gmail.com>
2026-08-17 22:35:58 +0300

    Testing dupes

--- tests/test_duck.py
+++ tests/test_duck.py
@@ -1,3 +1,5 @@
+from datetime import UTC, datetime
+
 import duckdb
 
 from pulsestream.storage.duck import write_batch
@@ -6,7 +8,17 @@ from tests.factories import create_WikiEvent
 
 def test_write_batch(con: duckdb.DuckDBPyConnection):
     write_batch(con, [create_WikiEvent()])
-    con.execute("SELECT * FROM wiki_events").fetchall()
+    rows = con.execute("SELECT * FROM wiki_events").fetchall()
+    assert len(rows) == 1
+    assert rows[0][0] == "e49b3bf0-04d9-4104-877f-64cbc2e5e9d9"
+    assert rows[0][1] == "enwiki"
+    assert rows[0][2] == datetime.fromtimestamp(1786622924, tz=UTC)
+
+
+def test_dupe_write(con: duckdb.DuckDBPyConnection):
+    write_batch(con, [create_WikiEvent()])
+    write_batch(con, [create_WikiEvent()])
+    assert con.execute("SELECT COUNT(*) FROM wiki_events").fetchall()[0][0] == 1
 
 
 def test_init_db(con: duckdb.DuckDBPyConnection):
